c7b9549622e884a20a177f674a231df0a746b62a
TatianeMarinho <devtatianemarinho@gmail.com>
2024-01-25 21:05:17 -0400

    teste encrypt_message

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,23 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
+
+message_crypt = 'buapau'
 
 
 def test_encrypt_message():
-    pass
+    # caso message nao seja string
+    with pytest.raises(TypeError):
+        encrypt_message(234, 2)
+
+    # caso o key nao seja um inteiro
+    with pytest.raises(TypeError):
+        encrypt_message(message_crypt, 'abc')
+
+    # caso key nao seja indice posit valido message, retorna message invertida
+    assert encrypt_message(message_crypt, 9) == 'uapaub'
+
+    # caso o key seja impar
+    assert encrypt_message(message_crypt, 3) == 'aub_uap'
+
+    # caso o key seja par
+    assert encrypt_message(message_crypt, 4) == 'ua_paub'
