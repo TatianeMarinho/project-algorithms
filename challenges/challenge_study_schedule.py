3fb59d00f960805048ca7aac8606c825fb04b084
TatianeMarinho <devtatianemarinho@gmail.com>
2024-01-25 20:29:49 -0400

    Algoritmo de busca

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,15 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    try:
+        student_count = 0
+        # percorrer a lista de periodos de permanencia
+        for period in permanence_period:
+            entry_time, exit_time = period
+            # verificar se o periodo de permanencia inclui o target_time
+            if entry_time <= target_time <= exit_time:
+                student_count += 1
+
+        return student_count
+
+    # caso o target_time nao esteja no formato esperado
+    except TypeError:
+        return None
