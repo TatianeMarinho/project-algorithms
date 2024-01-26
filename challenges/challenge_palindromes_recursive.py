248c6f2860185b4eb5fcce313c4aa3ad2c9b2275
TatianeMarinho <devtatianemarinho@gmail.com>
2024-01-25 22:15:56 -0400

    Palíndromos (Recursividade)

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,17 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    # se nao houver palavra
+    if not word:
+        return False
+
+    # coloca todas as letras em minusculo
+    word = word.lower()
+
+    # caso base
+    if low_index > high_index:
+        return True
+
+    # se a letra da posicao low_index for igual a letra da posicao high_index
+    if word[low_index] != word[high_index]:
+        return False
+
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
